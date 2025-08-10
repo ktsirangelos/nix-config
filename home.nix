11f1b1dfbf923911935c08d22a520f7ffa1d10fc
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2025-08-11 00:47:10 +0300

    Migrate packages from Brewfile to Nix

--- home.nix
+++ home.nix
@@ -6,7 +6,41 @@
   home.homeDirectory = "/Users/ktsirangelos";
 
   home.packages = [
+    pkgs.aspell
+    pkgs.automake
+    pkgs.bat
+    pkgs.cmake
+    pkgs.coreutils
+    pkgs.devspace
+    pkgs.diff-so-fancy
+    pkgs.fd
+    pkgs.fish
+    pkgs.fzf
+    pkgs.git
+    pkgs.kubernetes-helm
+    pkgs.imagemagick
+    pkgs.kubectl
+    pkgs.lftp
+    pkgs.libjpeg
+    pkgs.libpng
+    pkgs.libpq
     pkgs.neovim
+    pkgs.openssl
+    pkgs.php83Packages.php-cs-fixer
+    pkgs.pkg-config
+    pkgs.postgresql_17
+    pkgs.prettierd
+    pkgs.rbenv
+    pkgs.redis
+    pkgs.ripgrep
+    pkgs.sqlite
+    pkgs.tig
+    pkgs.tree
+    pkgs.nodePackages.typescript
+    pkgs.viu
+    pkgs.wdiff
+    pkgs.wget
+    pkgs.file
   ];
 
   programs.home-manager.enable = true;
@@ -25,6 +59,7 @@
   };
 
   xdg.configFile."nvim".source = ./configs/nvim;
+
   home.file.".aerospace.toml".source = ./configs/aerospace/aerospace.toml;
   home.file.".gitconfig".source = ./configs/git/gitconfig;
   home.file.".gitconfig.skroutz".source = ./configs/git/gitconfig.skroutz;
